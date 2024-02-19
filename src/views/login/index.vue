6e1f95132c33dd78af5338eda171174edf02e45e
xiaobin <xiaob3634@gmail.com>
2024-02-19 17:56:12 +0800

    feat: add login page

---
+++ src/views/login/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+onMounted(() => {
+})
+</script>
+
+<template>
+  <div relative wh-full>
+    <div class="box" />
+  </div>
+</template>
+
+<style scoped>
+.box{
+
+}
+</style>
